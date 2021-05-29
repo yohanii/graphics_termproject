4079265b03a417cb284797b353415041941cb42d
keechang <rlckd159g@gmail.com>
2021-05-29 18:19:11 +0900

    camera_follow

--- test0525/Assets/Scripts/PlayerMotor.cs
+++ test0525/Assets/Scripts/PlayerMotor.cs
@@ -10,7 +10,7 @@ public class PlayerMotor : MonoBehaviour
     private float speed = 5.0f;
     private float verticalVelocity = 0.0f;
     private float gravity = 12.0f;
-
+    private float animationDuration = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +20,12 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Time.time < animationDuration)
+        {
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+
         moveVector = Vector3.zero;
 
         if(controller.isGrounded)
